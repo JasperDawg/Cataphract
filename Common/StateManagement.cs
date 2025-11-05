af3a505442ca0af84031c66293ef0ceb60633707
Dreitone <gayforunova@gmail.com>
2025-11-05 17:09:29 -0600

    coding work for today

--- Common/StateManagement.cs
+++ Common/StateManagement.cs
@@ -1,16 +1,18 @@
+using System;
 using System.Collections.Generic;
 
 namespace Cataphract.Common.Utilities;
 
-public struct StateID
+public record StateID (string name, int id)
 {
-    internal string name;
-    internal int id;
+	public override string ToString() => string.IsNullOrEmpty(name) ? $"State#{id}" : $"{name}#{id}";
 }
+
+public readonly record struct StateEventArgs<T>(StateID StateId, bool Success, T[] Parameters) where T : struct;
 
 public abstract class State<T> where T : struct
 {
-    public StateID stateID;
+    public required StateID stateID;
     public T stateData;
     internal StateController<T>? enclosingController;
 
@@ -28,82 +30,99 @@ public abstract class State<T> where T : struct
     public abstract bool Exit(params T[] parameters);
     public abstract bool Update(params T[] parameters);
 
-    public bool PopSelf(params T[] parameters)
-    {
-        if (enclosingController != null)
-        {
-            Exit(parameters);
-            return enclosingController.PopState(stateID);
-        }
-        return false;
-    }
+	public bool IsActive => enclosingController?.CurrentState == this;
+
+	public bool PopSelf(params T[] parameters) =>
+		enclosingController?.PopState(stateID, parameters) ?? false;
 }
 
 public class StateController<T> where T : struct
 {
-    public StateController()
-    {
-        States = new Stack<State<T>>();
-    }
+	public StateController()
+	{
+		States = new Stack<State<T>>();
+	}
 
-    protected Stack<State<T>> States;
-    public State<T>? CurrentState => States.TryPeek(out var state) ? state : null;
+	protected Stack<State<T>> States;
+	public State<T>? CurrentState => States.TryPeek(out var state) ? state : null;
 
-    public delegate bool StateDelegate(StateID StateID, bool success, params T[] parameters);
-    public event StateDelegate? OnStatePush;
-    public event StateDelegate? OnStatePop;
+	public event EventHandler<StateEventArgs<T>>? StatePushed;
+	public event EventHandler<StateEventArgs<T>>? StatePopped;
 
-    int id = 0;
-    public bool PushState<S>(S state, params T[] arguments) where S : State<T>
-    {
-        if (state is State<T> newState)
-        {
-            newState.enclosingController = this;
-            newState.stateID = new StateID
-            {
-                name = state.GetType().Name,
-                id = id++
-            };
+	private int _nextId;
+	public bool PushState<S>(S state, params T[] arguments) where S : State<T>
+	{
+		if (state == null)
+			throw new ArgumentNullException(nameof(state));
 
-            States.Push(newState);
-            var output = newState.Enter(arguments);
-            return OnStatePush?.Invoke(newState.stateID, output, arguments) ?? true;
-        }
+		arguments ??= Array.Empty<T>();
+		state.enclosingController = this;
+		state.stateID = new StateID(state.GetType().Name, _nextId++);
 
-        return false;
-    }
-    public bool PopCurState()
-    {
-        if (States.TryPop(out var state))
-        {
-            return OnStatePop?.Invoke(state.stateID, state.Exit()) ?? true;
-        }
-        return false;
-    }
+		States.Push(state);
+		bool entered = state.Enter(arguments);
 
-    public bool PopState(StateID id)
-    {
-        if (States.TryPeek(out var currentState) && currentState.stateID.Equals(id))
-        {
-            return PopCurState();
-        }
-        return false;
-    }
-    public bool PopState(string name)
-    {
-        if (States.TryPeek(out var currentState) && currentState.stateID.name.Equals(name))
-        {
-            return PopCurState();
-        }
-        return false;
-    }
+		if (!entered)
+		{
+			States.Pop();
+			state.enclosingController = null;
+			state.stateID = default;
+			return false;
+		}
 
-    public bool Update(params T[] parameters)
-    {
-        if (States.TryPeek(out var currentState))
-        {
-            return currentState.Update(parameters);
-        }
-        return false;
-    }
+		StatePushed?.Invoke(this, new StateEventArgs<T>(state.stateID, true, arguments));
+		return true;
+	}
+
+	public bool TryPeek(out State<T> state) => States.TryPeek(out state);
+
+	public bool ReplaceState<S>(S state, params T[] arguments) where S : State<T>
+	{
+		if (!PopCurState(arguments))
+			return false;
+		return PushState(state, arguments);
+	}
+
+	public bool PopCurState(params T[] arguments)
+	{
+		arguments ??= Array.Empty<T>();
+		if (!States.TryPop(out var state))
+			return false;
+
+		bool exited = state.Exit(arguments);
+		state.enclosingController = null;
+		StatePopped?.Invoke(this, new StateEventArgs<T>(state.stateID, exited, arguments));
+		state.stateID = new StateID(string.Empty, -1);
+        
+		return exited;
+	}
+
+	public bool PopState(StateID id, params T[] arguments)
+	{
+		if (States.TryPeek(out var currentState) && currentState.stateID == id)
+			return PopCurState(arguments);
+		return false;
+	}
+
+	public bool PopState(string name, params T[] arguments)
+	{
+		if (States.TryPeek(out var currentState) && string.Equals(currentState.stateID.name, name, StringComparison.Ordinal))
+			return PopCurState(arguments);
+		return false;
+	}
+
+	public void Clear(params T[] arguments)
+	{
+		while (States.Count > 0)
+			PopCurState(arguments);
+	}
+
+	public bool Update(params T[] parameters)
+	{
+		if (States.TryPeek(out var currentState))
+		{
+			return currentState.Update(parameters);
+		}
+		return false;
+	}
 }
